ed9a1cd15f23cbac8f6e29921a7c9239e3f72765
CaldatoLuca <lucacaldato15@gmail.com>
2024-02-15 17:03:11 +0100

    feat: footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -3,7 +3,27 @@ export default {};
 </script>
 
 <template>
-  <footer>Footer</footer>
+  <footer>
+    <div class="container text-center py-3">
+      <h4>
+        Primo collegamento tra Backend e Frontend, facciamo una chiamata API
+        generata dal progetto
+        <a href="https://github.com/CaldatoLuca/laravel-api" target="_blank"
+          >Laravel Api</a
+        >
+      </h4>
+    </div>
+  </footer>
 </template>
 
-<style scoped></style>
+<style scoped lang="scss">
+@import "../scss/_partials/palette.scss";
+
+footer {
+  background-color: $red;
+  color: $black;
+  a {
+    color: $grey;
+  }
+}
+</style>
