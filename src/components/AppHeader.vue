96a59e3692156c5bbedf874bb41055dc97276a90
CaldatoLuca <lucacaldato15@gmail.com>
2024-02-16 17:58:11 +0100

    feat: router-link

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,5 +1,20 @@
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      pages: [
+        {
+          name: "home",
+          linkName: "Home",
+        },
+        {
+          name: "list",
+          linkName: "Projects",
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <template>
@@ -8,6 +23,15 @@ export default {};
       class="container d-flex justify-content-between align-items-center py-3"
     >
       <h1 class="title">Boolfolio</h1>
+      <div class="pages">
+        <ul class="list-unstyled d-flex gap-3 mb-0 align-items-center">
+          <li v-for="page in pages">
+            <RouterLink :to="{ name: page.name }">
+              {{ page.linkName }}</RouterLink
+            >
+          </li>
+        </ul>
+      </div>
       <h2 class="subtitle">Projects List API</h2>
     </div>
   </header>
@@ -22,8 +46,10 @@ header {
   .title {
     color: $black;
   }
-  .subtitle {
-    color: $grey;
+
+  a {
+    color: $red;
+    text-decoration: none;
   }
 }
 </style>
