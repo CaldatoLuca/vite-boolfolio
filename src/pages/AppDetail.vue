9f48615af992e5217561859d6c4e8aad0bcb4d5c
CaldatoLuca <lucacaldato15@gmail.com>
2024-02-17 12:01:59 +0100

    feat: detail page con dati

--- src/pages/AppDetail.vue
+++ src/pages/AppDetail.vue
@@ -1,9 +1,43 @@
 <script>
-export default {};
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      project: {},
+      baseUrl: "http://127.0.0.1:8000",
+      apiUrls: {
+        projects: "/api/projects",
+      },
+    };
+  },
+  methods: {
+    getProject() {
+      axios
+        .get(
+          this.baseUrl + this.apiUrls.projects + "/" + this.$route.params.slug
+        )
+        .then((response) => {
+          this.project = response.data.data;
+          console.log(this.project);
+        })
+        .catch((error) => console.log(error));
+    },
+  },
+  created() {
+    this.getProject();
+  },
+};
 </script>
 
 <template>
-  <div>Single project</div>
+  <main>
+    <div class="container py-5">
+      <h1 class="mb-4">{{ project.title }}</h1>
+      <p class="mb-3">{{ project.description }}</p>
+      <div class="mb-3">{{ project.type.title }}</div>
+    </div>
+  </main>
 </template>
 
 <style></style>
