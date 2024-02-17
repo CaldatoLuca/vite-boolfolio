c2862e8b7b1a43ddd5f0fbbe1eeaf4e126342230
CaldatoLuca <lucacaldato15@gmail.com>
2024-02-17 10:51:09 +0100

    feat: catch all route

---
+++ src/pages/NotFound.vue
@@ -0,0 +1,9 @@
+<script>
+export default {};
+</script>
+
+<template>
+  <main>Pagina non trovata</main>
+</template>
+
+<style></style>
--- src/router.js
+++ src/router.js
@@ -5,6 +5,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import AppHome from "./pages/AppHome.vue";
 import AppAbout from "./pages/AppAbout.vue";
 import ProjectList from "./pages/ProjectList.vue";
+import NotFound from "./pages/NotFound.vue";
 
 const router = createRouter({
   history: createWebHistory(),
@@ -23,6 +24,11 @@ const router = createRouter({
       path: "/projects",
       name: "list",
       component: ProjectList,
+    },
+    {
+      path: "/:pathMatch(.*)*",
+      name: "not-found",
+      component: NotFound,
     },
   ],
 });
